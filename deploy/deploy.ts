e2685a6a5aed96da1d77c74c1e839398430938bc
clairemoorew <PolitowskiDevery57@hotmail.com>
2025-10-22 15:56:03 +0800

    deploy setup

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedAccessControl = await deploy("EncryptedAccessControl", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedAccessControl contract: `, deployedAccessControl.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encrypted_access_control"; // id required to prevent reexecution
+func.tags = ["EncryptedAccessControl"];
